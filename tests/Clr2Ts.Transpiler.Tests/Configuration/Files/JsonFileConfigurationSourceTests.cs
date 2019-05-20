edbc8d5cd417e5695af48f5c82cbae4b2d9185de
Dennis Janiak <dennis.janiak@googlemail.com>
2019-05-20 20:45:15 +0200

    Add tests for the JsonFileConfiguration.

--- tests/Clr2Ts.Transpiler.Tests/Configuration/Files/JsonFileConfigurationSourceTests.cs
+++ tests/Clr2Ts.Transpiler.Tests/Configuration/Files/JsonFileConfigurationSourceTests.cs
@@ -1,7 +1,9 @@
 ﻿using Clr2Ts.Transpiler.Configuration;
 using Clr2Ts.Transpiler.Configuration.Files;
 using Clr2Ts.Transpiler.Input;
+using System;
 using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace Clr2Ts.Transpiler.Tests.Configuration.Files
@@ -34,6 +36,108 @@ namespace Clr2Ts.Transpiler.Tests.Configuration.Files
             var config = JsonFileConfigurationSource.FromFile("configuration.sample.json");
 
             Assert.Single(config.GetSection<InputConfiguration>().AssemblyFiles);
+        }
+
+        /// <summary>
+        /// An <see cref="InvalidOperationException"/> should be thrown when
+        /// trying to retreive a config section with an unsupported representation type.
+        /// </summary>
+        [Fact]
+        public void JsonFileConfigurationSource_GetSectionByKeyThrowsInvalidOperationException()
+        {
+            var config = JsonFileConfigurationSource.FromFile("configuration.sample.json");
+
+            Assert.Throws<InvalidOperationException>(() =>
+                config.GetSection<UnconfiguredConfigSection>());
+        }
+
+        /// <summary>
+        /// Exceptions that occur by deserializing the config section should
+        /// be wrapped in a <see cref="ConfigurationException"/>.
+        /// </summary>
+        [Fact]
+        public void JsonFileConfigurationSource_GetSectionByKeyThrowsConfigurationException()
+        {
+            var config = JsonFileConfigurationSource.FromFile("configuration.sample.json");
+
+            var exception = Assert.Throws<ConfigurationException>(() =>
+                config.GetSection<UnreadableConfigSection>());
+            Assert.NotNull(exception.InnerException);
+        }
+
+        /// <summary>
+        /// The ConfigurationSource should return a default value for missing sections (null).
+        /// </summary>
+        [Fact]
+        public void JsonFileConfigurationSource_GetSectionByKeyReturnsDefaultValue()
+        {
+            var config = JsonFileConfigurationSource.FromFile("configuration.sample.json");
+
+            Assert.Null(config.GetSection<MissingConfigSection>());
+        }
+
+        /// <summary>
+        /// Configuration files can be found by a naming convention.
+        /// </summary>
+        [Fact]
+        public void JsonFileConfigurationSource_SearchesFileInDirectory()
+        {
+            var config = JsonFileConfigurationSource.FromDirectory("Configuration/Files/SampleDirectoryWithConfigFile");
+
+            Assert.Equal("testcase-sample-directory-with-configfile", 
+                config.GetSection<InputConfiguration>().AssemblyFiles.Single());
+        }
+
+        /// <summary>
+        /// When searching for a configuration file by its name,
+        /// the search should look in parent directories of the specified directory.
+        /// </summary>
+        [Fact]
+        public void JsonFileConfigurationSource_SearchesFileInParentDirectories()
+        {
+            var config = JsonFileConfigurationSource.FromDirectory("Configuration/Files/SampleDirectoryWithSubDirectory/SampleSubDirectoryWithConfigFile");
+
+            Assert.Equal("testcase-sample-subdirectory-with-configfile",
+                config.GetSection<InputConfiguration>().AssemblyFiles.Single());
+        }
+
+
+        /// <summary>
+        /// A <see cref="ConfigurationException"/> should be thrown when multiple
+        /// configuration files match the naming convention in the same directory.
+        /// </summary>
+        [Fact]
+        public void JsonFileConfigurationSource_MultipleSearchMatchThrowsConfigurationException()
+        {
+            Assert.Throws<ConfigurationException>(
+                () => JsonFileConfigurationSource.FromDirectory("Configuration/Files/SampleDirectoryWithMultipleConfigFiles"));
+        }
+
+        /// <summary>
+        /// A <see cref="ConfigurationException"/> should be thrown when no
+        /// configuration files match the naming convention, looking upwards from the specified directory.
+        /// </summary>
+        [Fact]
+        public void JsonFileConfigurationSource_NoSearchMatchThrowsConfigurationException()
+        {
+            Assert.Throws<ConfigurationException>(
+                () => JsonFileConfigurationSource.FromDirectory("Configuration/Files/SampleDirectoryWithoutConfigFile"));
+        }
+
+        // Missing attribute.
+        private class UnconfiguredConfigSection
+        { }
+
+        [ConfigurationSection("input")]
+        private class UnreadableConfigSection
+        {
+            public int AssemblyFiles { get; set; }
+        }
+
+        [ConfigurationSection("missing")]
+        private class MissingConfigSection
+        {
+            public int SomeValue { get; set; } = 5;
         }
     }
 }
