f228632f08f31fdd6acfc76ea5a880e2a31848a0
Dennis Janiak <dennis.janiak@googlemail.com>
2022-03-01 10:18:59 +0100

    Create sample project for net6.

---
+++ Clr2Ts.EndToEnd.Net6.Targets/ExampleClass1.cs
@@ -0,0 +1,34 @@
+﻿namespace Clr2Ts.EndToEnd.Targets
+{
+    /// <summary>
+    /// Simple example class type for testing basic
+    /// compatibility with .NET 6 libraries.
+    /// </summary>
+    public class ExampleClass1
+    {
+        /// <summary>
+        /// Simple string property.
+        /// </summary>
+        public string? SomeProperty { get; set; }
+
+        /// <summary>
+        /// Property with type Guid.
+        /// </summary>
+        public Guid SomeGuidProperty { get; set; }
+
+        /// <summary>
+        /// Property with type boolean.
+        /// </summary>
+        public bool SomeBooleanProperty { get; set; }
+
+        /// <summary>
+        /// Property with type DateTime.
+        /// </summary>
+        public DateTime SomeDateTimeProperty { get; set; }
+
+        /// <summary>
+        /// Property with type Int32.
+        /// </summary>
+        public int SomeIntegerProperty { get; set; }
+    }
+}
